b366f0fd3413d2eba54bed2a64606a329c6c412f
Antonio Hernandez Souza <github@losinfluyentes.com>
2016-01-20 19:27:26 +0000

    20160120192726

--- actualizar.sh
+++ actualizar.sh
@@ -1,14 +1,6 @@
 #!/bin/bash          
-    fecha= $1
     now=$(date +%Y%m%d%H%M%S)
-    echo on
     git add .
-    if [$fecha]; then
-        git commit -m$1
-    echo "Archivos actualizados al "+$1
-    else
-        git commit -m$now
-    echo "Archivos actualizados al "+$now
-    fi
-    echo off
+    git commit -m$now
+    echo "Archivos actualizados al "$now
     #git pull
